40d61b702882f76ace9a0f3f57663e015654b7f9
Miguel Salazar <yobebe@hotmail.com.ar>
2026-02-20 00:03:39 -0300

    Bug solucionado, el problema era una letra de mas

--- src/features/products/ProductGrid/ProductGrid.jsx
+++ src/features/products/ProductGrid/ProductGrid.jsx
@@ -1,26 +1,21 @@
-import style from './ProductGrid.module.scss';
-import ProductsCard from '../components/ProductCard.jsx';
-import useProducts from '../hooks/useProducts.jsx';
+import style from "./ProductGrid.module.scss";
+import ProductCard from "../components/ProductCard.jsx";
+import useProducts from "../hooks/useProducts.jsx";
 
 const ProductGrid = () => {
   const { products, loading } = useProducts();
 
-  if( loading ) return <p> Cargando productos... </p>;
+  if (loading) return <p> Cargando productos... </p>;
 
-    return (
+  return (
     <>
-        <section className={ style.grid }>
-            {
-                products.map(( product ) => {
-                    <ProductsCard
-                        hey={ product.id }
-                        product={ product }
-                    />
-                })
-            }
-        </section>
+      <section className={style.grid}>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default ProductGrid;
