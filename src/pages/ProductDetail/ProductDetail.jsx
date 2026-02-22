5a2baf956aecc5422e9c0ab2ab0f096ceca0f4d1
Miguel Salazar <yobebe@hotmail.com.ar>
2026-02-21 23:17:11 -0300

    Problema aparentemente solucionado

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { useCart } from "../../context/context/CartContext";
+import { useCart } from "../../features/cart/hooks/useCart";
 import { getProducts } from "../../features/products/services/productsApi";
+import { useEffect, useState } from "react";
 import styles from "./ProductDetail.module.scss";
 
 const ProductDetail = () => {
@@ -8,7 +9,21 @@ const ProductDetail = () => {
   const navigate = useNavigate();
   const { addToCart } = useCart();
 
-  const product = getProducts.find((p) => p.id === Number(id));
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadProduct = async () => {
+      const products = await getProducts(); // ← ejecutás la función
+      const found = products.find((p) => p.id === Number(id));
+      setProduct(found);
+      setLoading(false);
+    };
+
+    loadProduct();
+  }, [id]);
+
+  if (loading) return <p>Cargando producto...</p>;
 
   if (!product) {
     return (
@@ -25,23 +40,13 @@ const ProductDetail = () => {
     <section className={styles.container}>
       <div className={styles.grid}>
         <div className={styles.imageWrapper}>
-          <img
-            src={product.image}
-            alt={product.title}
-            className={styles.image}
-          />
+          <img src={product.image} alt={product.title} className={styles.image} />
         </div>
 
         <div className={styles.info}>
           <h1 className={styles.title}>{product.title}</h1>
 
-          <p className={styles.description}>
-            {product.description}
-          </p>
-
-          <span className={styles.price}>
-            ${product.price}
-          </span>
+          <span className={styles.price}>${product.price}</span>
 
           <button
             className={styles.addButton}
